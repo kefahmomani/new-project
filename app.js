bebbd82fefe947f64bdd3cc49c206cde8ec22be9
kefahJordan <>
2021-04-14 17:33:59 +0000

    lab

---
+++ app.js
@@ -0,0 +1,14 @@
+alert("Hello! welcome to jordan ^.^ ");
+
+var city = prompt("Please enter the name of city in jordan you want to visit ", "PATRA ");
+
+if (city == "PATRA" ) {
+ alert("I wish you an enjoyable watch ^.^ ");
+} else if (city == "AQUABA" ) {
+   alert("I wish you an enjoyable watch ^.^ ");
+} else {
+   alert("I'm sorry, You will not find what you are looking for on my website  ");
+}
+
+document.write("I hope you enjoyed browsing my website ^.^");
+
