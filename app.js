eec931f429f80b8e4ac46e480e55efd7dbe713fc
kefahJordan <>
2021-04-15 17:05:37 +0000

    add

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-alert("Hello! welcome to jordan ^.^ ");
+var kefaName = prompt('Hello welcome, please enter your name !');
 
-var city = prompt("Please enter the name of city in jordan you want to visit ", "PATRA ");
-
-if (city == "PATRA" ) {
- alert("I wish you an enjoyable watch ^.^ ");
-} else if (city == "AQUABA" ) {
-   alert("I wish you an enjoyable watch ^.^ ");
-} else {
-   alert("I'm sorry, You will not find what you are looking for on my website  ");
+// validator 
+// i will use the and
+while(kefaName !== 'petra'&& kefaName !== 'aquaba'){
+  kefaName = prompt('Please enter the name petra or aquaba');
 }
 
-document.write("I hope you enjoyed browsing my website ^.^");
+// var will save the image that the user choose
+var userChoose;
 
+if(kefaName === 'petra'){
+userChoose='<img src="https://images.memphistours.com/large/184203755_Petra.jpg">';
+}else if (kefaName === 'aquaba'){
+  userChoose='<img src="https://blog.padi.com/wp-content/uploads/2013/04/AAD-Pictur-64.jpg">';
+
+ 
+  
+}
+
+
+var imagesNumber= prompt('How many image do you want???');
+
+for(var i=0 ; i < imagesNumber; i++){
+  document.write(userChoose);
+}
